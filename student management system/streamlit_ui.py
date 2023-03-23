3cf2c5268e2bcd4cf777821ce228c2f05567d3ff
Sanowar Dewan <sanowardwnofficial@gmail.com>
2023-03-23 18:24:13 +0600

    test

--- student management system/streamlit_ui.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-from Contract_Deployment import *
-
-student_id = st.text_input("Enter the student id")
-if st.button('Get Student'):
-    student_id = int(student_id)
-    st.success(getStudents(student_id))
